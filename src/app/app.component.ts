273d383faa26c76edf0bf0a75712f6515c28e15f
Angel <sanchezangel.16@outlook.com>
2025-11-03 17:30:29 -0600

    actualicé las imagenes y compuse la galeria completa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,41 +25,11 @@ export class AppComponent implements OnInit {
   currentImageIndex: number = 0;
 
   projectsListToShow: Project[] = [
-    {
-      title: 'Lotería Mexicana',
-      description:
-        'Proyecto full-stack (Next.js + Node/Express) que permite jugar Lotería mexicana en línea mediante WebSockets. Los usuarios pueden crear salas privadas, unirse mediante código, ver a otros jugadores conectados y su estado, y jugar en tiempo real con emisión de cartas desde el servidor. El frontend está desplegado en Vercel y el backend en una instancia EC2 con Nginx.',
-      paragraphs: [
-        // 'El juego permite a los usuarios crear salas y compartir un código único para que otros jugadores se unan, visualizar los nombres de los participantes y prepararse para la partida. Una vez que todos los jugadores se marcan como “listos”, el servidor inicia la partida, emitiendo las cartas de manera sincronizada.',
-        // 'Los jugadores van marcando sus cartas a medida que aparecen, y cuando un jugador logra Lotería, puede reclamarla mediante un botón. El servidor valida la jugada y proclama al ganador, asegurando una experiencia de juego justa y en tiempo real.',
-        // 'El proyecto destaca por su interactividad multijugador, sincronización precisa de eventos y recreación digital de un juego tradicional mexicano. Fue desplegado profesionalmente, con el backend en una instancia EC2 de AWS y la aplicación Next.js en Vercel, demostrando habilidades en desarrollo full stack y despliegue en la nube.',
-      ],
-      images: [
-        'images/projects/loteria/03.png',
-        'images/projects/loteria/01.png',
-        'images/projects/loteria/02.png',
-      ],
-      currentImageIndex: 0,
-      techList: [
-        { icon: 'nextjs', title: 'NextJS' },
-        { icon: 'tailwindcss', title: 'TailwindCSS' },
-        { icon: 'nodejs', title: 'NodeJS' },
-        { icon: 'express', title: 'Express' },
-        { icon: 'aws', title: 'AWS' },
-        { icon: 'vercel', title: 'Vercel' },
-      ],
-      githubLink: 'https://github.com/angelsr16/loteria',
-      categories: [DESTACADOS],
-    },
+    // Substracker
     {
       title: 'Control de Suscripciones',
       description:
         'Proyecto full stack desarrollado con Next.js en el frontend y Node.js + MongoDB en el backend, diseñado para administrar y recordar suscripciones de manera segura y automatizada. Los usuarios pueden registrar sus suscripciones con detalles como precio, frecuencia y método de pago, y reciben recordatorios automáticos por correo electrónico antes de la fecha de renovación.',
-      paragraphs: [
-        'El sistema implementa autenticación mediante JWT (JSON Web Tokens) para garantizar sesiones seguras y confiables, además de un proceso de registro validado por códigos OTP gestionados con Redis (Upstash).',
-        'Los usuarios pueden registrar sus suscripciones con detalles como precio, frecuencia y método de pago, y reciben recordatorios automáticos por correo electrónico antes de la fecha de renovación. El sistema destaca por su automatización de notificaciones, seguridad en la autenticación, gestión eficiente de datos y experiencia de usuario intuitiva.',
-        'El proyecto fue desplegado profesionalmente, con el frontend alojado en Vercel y el backend en una instancia EC2 de AWS. Además, integra varios servicios de AWS para garantizar escalabilidad y disponibilidad, incluyendo AWS SQS para la gestión de colas de envío de correos, y AWS EventBridge junto con una Lambda en Python para ejecutar diariamente los procesos de recordatorio.',
-      ],
       images: [
         'images/projects/substracker/01.png',
         'images/projects/substracker/02.png',
@@ -101,16 +71,59 @@ export class AppComponent implements OnInit {
       githubLink: 'https://github.com/angelsr16/subscription-tracker',
       categories: [DESTACADOS],
     },
+    // Obras
+    {
+      title: 'Sistema de Obras Públicas',
+      description:
+        'Plataforma web destinada a mejorar la transparencia y el acceso a la información pública en el ámbito del gobierno municipal. El sistema permite gestionar y visualizar de forma centralizada todas las obras públicas del municipio, incluyendo datos clave como montos ejecutados, ubicación geográfica, rubros de inversión, fuente de financiamiento, área ejecutora y año de ejecución.',
+
+      images: [
+        'images/projects/obras/01.jpg',
+        'images/projects/obras/02.jpg',
+        'images/projects/obras/03.jpg',
+        'images/projects/obras/04.jpg',
+        'images/projects/obras/05.jpg',
+      ],
+      currentImageIndex: 0,
+      techList: [
+        { icon: 'angular', title: 'Angular' },
+        { icon: 'tailwind', title: 'TailwindCSS' },
+        { icon: 'firebase', title: 'Firebase' },
+      ],
+      githubLink: '',
+      categories: [DESTACADOS, SISTEMAS],
+    },
+    // Lotería
+    {
+      title: 'Lotería Mexicana',
+      description:
+        'Proyecto full-stack (Next.js + Node/Express) que permite jugar Lotería mexicana en línea mediante WebSockets. Los usuarios pueden crear salas privadas, unirse mediante código, ver a otros jugadores conectados y su estado, y jugar en tiempo real con emisión de cartas desde el servidor. El frontend está desplegado en Vercel y el backend en una instancia EC2 con Nginx.',
+      images: [
+        'images/projects/loteria/03.jpg',
+        'images/projects/loteria/01.jpg',
+        'images/projects/loteria/02.jpg',
+      ],
+      currentImageIndex: 0,
+      techList: [
+        { icon: 'nextjs', title: 'NextJS' },
+        { icon: 'tailwindcss', title: 'TailwindCSS' },
+        { icon: 'nodejs', title: 'NodeJS' },
+        { icon: 'express', title: 'Express' },
+        { icon: 'aws', title: 'AWS' },
+        { icon: 'vercel', title: 'Vercel' },
+      ],
+      githubLink: 'https://github.com/angelsr16/loteria',
+      categories: [DESTACADOS],
+    },
+    // Connect 4
     {
       title: 'Conecta 4 Online',
       description:
         'Desarrollo full stack con FastAPI, Angular y MongoDB que permite jugar de forma local u online mediante la creación de partidas privadas con código de acceso. Implementa sistema de puntuación ELO y un leaderboard dinámico con el top 10 de jugadores, ofreciendo una experiencia competitiva y en tiempo real.',
       images: [
-        'images/projects/obras/sistema_obras_01.png',
-        'images/projects/obras/sistema_obras_02.png',
-        'images/projects/obras/sistema_obras_03.png',
-        'images/projects/obras/sistema_obras_04.png',
-        'images/projects/obras/sistema_obras_05.png',
+        'images/projects/connect-4/01.jpg',
+        'images/projects/connect-4/02.jpg',
+        'images/projects/connect-4/03.jpg',
       ],
       currentImageIndex: 0,
       techList: [
@@ -126,27 +139,6 @@ export class AppComponent implements OnInit {
       ],
       githubLink: 'https://github.com/angelsr16/online-connect-4',
       categories: [DESTACADOS],
-    },
-    {
-      title: 'Sistema de Obras Públicas',
-      description:
-        'Plataforma web destinada a mejorar la transparencia y el acceso a la información pública en el ámbito del gobierno municipal. El sistema permite gestionar y visualizar de forma centralizada todas las obras públicas del municipio, incluyendo datos clave como montos ejecutados, ubicación geográfica, rubros de inversión, fuente de financiamiento, área ejecutora y año de ejecución.',
-
-      images: [
-        'images/projects/obras/sistema_obras_01.png',
-        'images/projects/obras/sistema_obras_02.png',
-        'images/projects/obras/sistema_obras_03.png',
-        'images/projects/obras/sistema_obras_04.png',
-        'images/projects/obras/sistema_obras_05.png',
-      ],
-      currentImageIndex: 0,
-      techList: [
-        { icon: 'angular', title: 'Angular' },
-        { icon: 'tailwind', title: 'TailwindCSS' },
-        { icon: 'firebase', title: 'Firebase' },
-      ],
-      githubLink: '',
-      categories: [DESTACADOS, SISTEMAS],
     },
   ];
 
@@ -175,27 +167,16 @@ export class AppComponent implements OnInit {
       (project.currentImageIndex + 1) % project.images.length;
   }
 
-  // filterProjects(category: number) {
-  //   this.filterData.category = category;
-  //   this.projectsListToShow = this.projects.filter((project) => {
-  //     if (this.filterData.category === TODOS) return true;
-  //     return project.categories.includes(this.filterData.category);
-  //   });
-  // }
+  prevGalleryImage() {
+    if (!this.currentProjectGallery) return;
+    this.currentImageIndex =
+      (this.currentImageIndex - 1 + this.currentProjectGallery.images.length) %
+      this.currentProjectGallery.images.length;
+  }
 
-  // handlePreviousImage(project: Project) {
-  //   if (project.currentImageIndex - 1 === -1) {
-  //     project.currentImageIndex = project.images.length - 1;
-  //   } else {
-  //     project.currentImageIndex -= 1;
-  //   }
-  // }
-
-  // handleNextImage(project: Project) {
-  //   if (project.currentImageIndex + 1 === project.images.length) {
-  //     project.currentImageIndex = 0;
-  //   } else {
-  //     project.currentImageIndex += 1;
-  //   }
-  // }
+  nextGalleryImage() {
+    if (!this.currentProjectGallery) return;
+    this.currentImageIndex =
+      (this.currentImageIndex + 1) % this.currentProjectGallery.images.length;
+  }
 }
--- src/app/models/Project.ts
+++ src/app/models/Project.ts
@@ -1,15 +1,11 @@
 export interface Project {
   title: string;
   description: string;
-  paragraphs?: string[];
-  displayParagraph?: boolean;
   images: string[];
   currentImageIndex: number;
   techList: TechItem[];
   githubLink: string;
   categories: number[];
-  websiteLink?: string;
-  websiteTitle?: string;
 }
 
 export interface TechItem {
